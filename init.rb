33a821421b9a1291e3f439c3a0ad5e103896ddab
Adam Palmblad <adam.palmblad@teampages.com>
2010-07-28 16:44:38 -0700

    Fix up init.rb

--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
 # Include hook code here
 require 'active_record/sql_exporter'
 ActiveRecord::Base.send( :include, ActiveRecord::SqlExporter::InstanceMethods )
+ActiveRecord::Base.send( :extend, ActiveRecord::SqlExporter::ClassMethods )
