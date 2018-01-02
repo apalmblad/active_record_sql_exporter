f1dbdf1aa729929d5bf479183db2e1208222a018
Adam Palmblad <adam.palmblad@teampages.com>
2018-01-02 09:34:33 -0800

    Formatting.

--- lib/active_record/sql_exporter.rb
+++ lib/active_record/sql_exporter.rb
@@ -2,7 +2,7 @@
 module ActiveRecord::SqlExporter
   class NestedException < ArgumentError
     attr_accessor :old_exception, :klass
-    def initialize( old_exception, klass, key )
+    def initialize(old_exception, klass, key)
       @old_exception = old_exception
       @klass = klass
       @key = key
@@ -29,20 +29,20 @@ module ActiveRecord::SqlExporter
   ##############################################################################
   class FileWriter
     # --------------------------------------------------------------- initialize
-    def initialize( file )
+    def initialize(file)
       @file = file
     end
     # ------------------------------------------------------------------------ +
-    def +( s )
-      @file.write( s )
+    def +(s)
+      @file.write(s)
       return self
     end
   end
 
   module ClassMethods
     # ---------------------------------------------------------- build_check_sql
-    def build_check_sql( id )
-      "IF( NOT EXISTS( SELECT * FROM #{quoted_table_name} WHERE #{connection.quote_column_name(primary_key)} = #{quote_value(id, columns_hash[primary_key])} ) THEN ROLLBACK; END IF;\n"
+    def build_check_sql(id)
+      "IF( NOT EXISTS( SELECT * FROM #{quoted_table_name} WHERE #{connection.quote_column_name(primary_key)} = #{quote_value(id, columns_hash[primary_key])}) THEN ROLLBACK; END IF;\n"
     end
   end
 
@@ -50,17 +50,17 @@ module ActiveRecord::SqlExporter
     CREATION_NODE = 1
     EXISTENCE_CHECK_NODE = 2
     UPDATE_NODE = 3
-    # --------------------- pretend_to_sql( args = {}, classes_to_ignore = [] ) 
-    def print_relation_tree( args = {}, classes_to_ignore = [] ) 
+    # --------------------- pretend_to_sql(args = {}, classes_to_ignore = []) 
+    def print_relation_tree(args = {}, classes_to_ignore = []) 
       tree = {}
-      _print_relation( tree, classes_to_ignore )
-      return if classes_to_ignore.include?( self.class )
+      _print_relation(tree, classes_to_ignore)
+      return if classes_to_ignore.include?(self.class)
     end
     # ------------------------------------------------------------------- to_sql
-    def to_backup_sql( args = {}, classes_to_ignore = [] )
+    def to_backup_sql(args = {}, classes_to_ignore = [])
       tree = {}
-      build_export_tree( tree, classes_to_ignore )
-      sql = args[:file] ? ActiveRecord::SqlExporter::FileWriter.new( args[:file] ) : ''
+      build_export_tree(tree, classes_to_ignore)
+      sql = args[:file] ? ActiveRecord::SqlExporter::FileWriter.new(args[:file]) : ''
       unless args[:no_transaction]
         sql += "BEGIN;"
       end
@@ -69,16 +69,16 @@ module ActiveRecord::SqlExporter
           node = tree[klass][id]
           begin
             if node[:type] == EXISTENCE_CHECK_NODE
-              sql += klass.constantize.build_check_sql( id )
+              sql += klass.constantize.build_check_sql(id)
             elsif node[:type] == CREATION_NODE
               if id
-                object = klass.constantize.find( id )
+                object = klass.constantize.find(id)
                 sql += object.sql_restore_string
               end
             elsif node[:type] == UPDATE_NODE
               if id
-                object = klass.constantize.find( id )
-                sql += object.update_sql_string( node[:key] )
+                object = klass.constantize.find(id)
+                sql += object.update_sql_string(node[:key])
               end
             end
           rescue Encoding::UndefinedConversionError
@@ -89,15 +89,17 @@ module ActiveRecord::SqlExporter
       sql += "COMMIT;" unless args[:no_transaction]
       return sql
     end
-################################################################################
-    protected
-################################################################################
+  ##############################################################################
+
+  protected
+
+  ##############################################################################
     # -------------------------------------------------------- update_sql_string
-    def update_sql_string( key_name )
+    def update_sql_string(key_name)
       data = []
       self.class.columns.map do |x|
         next if is_primary_key_field?(x)
-        data << "#{self.class.connection.quote_column_name( x.name )}=#{self.class.connection.quote( read_attribute(x.name))}"
+        data << "#{self.class.connection.quote_column_name(x.name)}=#{self.class.connection.quote(read_attribute(x.name))}"
       end
       quoted_id = self.class.quote_value(id, self.class.columns_hash[self.class.primary_key])
       "UPDATE #{self.class.quoted_table_name} SET #{data.join(',')} WHERE #{self.class.connection.quote_column_name(self.class.primary_key)} = #{quoted_id};\n"
@@ -108,12 +110,12 @@ module ActiveRecord::SqlExporter
     end
 
     # ------------------------------------------------------- sql_restore_string
-    def sql_restore_string( args = {} )
+    def sql_restore_string(args = {})
       columns = self.class.columns.map do |x|
-        self.class.connection.quote_column_name( x.name )
+        self.class.connection.quote_column_name(x.name)
       end
       values = self.class.columns.map do |x|
-        self.class.connection.quote( read_attribute( x.name ) )
+        self.class.connection.quote(read_attribute(x.name))
       end
 
       sql = "\nINSERT INTO #{self.class.quoted_table_name} (#{columns.join(',')}) VALUES (#{values.join(',')})"
@@ -128,16 +130,16 @@ module ActiveRecord::SqlExporter
       return sql
     end
     # -------------------------------------------------------- build_export_tree
-    def build_export_tree( tree = {}, classes_to_ignore = [] )
-      return if classes_to_ignore.include?( self.class )
-      if tree[self.class.name].nil? || ( tree[self.class.name] && ( tree[self.class.name][id].nil? || tree[self.class.name][id][:type] == EXISTENCE_CHECK_NODE ) )
-        self.add_to_tree( tree, CREATION_NODE )
-        expand_tree_with_relations( tree, self.class.reflections, classes_to_ignore )
+    def build_export_tree(tree = {}, classes_to_ignore = [])
+      return if classes_to_ignore.include?(self.class)
+      if tree[self.class.name].nil? || (tree[self.class.name] && (tree[self.class.name][id].nil? || tree[self.class.name][id][:type] == EXISTENCE_CHECK_NODE))
+        self.add_to_tree(tree, CREATION_NODE)
+        expand_tree_with_relations(tree, self.class.reflections, classes_to_ignore)
       end
       return tree
     end
     # ------------------------------------------------------------- add_to_tree 
-    def add_to_tree( tree, type, options = {} )
+    def add_to_tree(tree, type, options = {})
       tree[self.class.name] ||= {}
       node = tree[self.class.name][self.id] 
       if node.nil? || node[:type] == EXISTENCE_CHECK_NODE
@@ -148,17 +150,17 @@ module ActiveRecord::SqlExporter
     end
     # ---------------------------------------------------------- build_check_sql
     def build_check_sql
-      "IF( NOT EXISTS( SELECT * FROM #{self.class.quoted_table_name} WHERE #{self.class.connection.quote_column_name(self.class.primary_key)} = #{quote_value(id)} ) THEN ROLLBACK; END IF;\n"
+      "IF( NOT EXISTS( SELECT * FROM #{self.class.quoted_table_name} WHERE #{self.class.connection.quote_column_name(self.class.primary_key)} = #{quote_value(id)}) THEN ROLLBACK; END IF;\n"
     end
     # ---------------------------------------- convert_has_many_relations_to_sql
-    def expand_tree_with_relations( tree, reflections, classes_to_ignore )
+    def expand_tree_with_relations(tree, reflections, classes_to_ignore)
       reflections.each_pair do |key, value|
-        next if value.options[:dependent] && ![:destroy, :nullify].include?( value.options[:dependent] )
+        next if value.options[:dependent] && ![:destroy, :nullify].include?(value.options[:dependent])
         if value.options[:polymorphic]
-          next if classes_to_ignore.include?( send( key ).class )
+          next if classes_to_ignore.include?(send(key).class)
         else
           begin
-            next if classes_to_ignore.include?( value.klass )
+            next if classes_to_ignore.include?(value.klass)
           rescue
             raise "Problem in a #{self.class.name} with #{key} = #{value}"
           end
@@ -166,33 +168,33 @@ module ActiveRecord::SqlExporter
         case value.macro
         when :has_one
           begin
-            singleton_method( key ) do |e|
-              e.build_export_tree( tree, classes_to_ignore )
+            singleton_method(key) do |e|
+              e.build_export_tree(tree, classes_to_ignore)
             end
           rescue NestedException => ex
-            raise NestedException.new( ex.old_exception, "#{self.class.name}.#{ex.klass}", key )
+            raise NestedException.new(ex.old_exception, "#{self.class.name}.#{ex.klass}", key)
           rescue Exception => ex
-            raise NestedException.new( ex, self.class.name, key )
+            raise NestedException.new(ex, self.class.name, key)
           end
         when :has_many, :has_and_belongs_to_many
           begin
-            records = send( key )
+            records = send(key)
             if value.options[:dependent] == :nullify
               records.each do |record|
-                record.add_to_tree( tree, UPDATE_NODE, key: value.association_primary_key )
+                record.add_to_tree(tree, UPDATE_NODE, key: value.association_primary_key)
               end
             else
-              records.each{ |x| x.build_export_tree( tree, classes_to_ignore ) }
+              records.each{ |x| x.build_export_tree(tree, classes_to_ignore) }
             end
           rescue NestedException => ex
-            raise NestedException.new( ex.old_exception, "#{self.class.name}.#{ex.klass}", key )
+            raise NestedException.new(ex.old_exception, "#{self.class.name}.#{ex.klass}", key)
           rescue Exception => ex
             raise ex
-            raise NestedException.new( ex, self.class.name, key )
+            raise NestedException.new(ex, self.class.name, key)
           end
         when :belongs_to
-          singleton_method( key ) do |e|
-            e.add_to_tree( tree, EXISTENCE_CHECK_NODE )
+          singleton_method(key) do |e|
+            e.add_to_tree(tree, EXISTENCE_CHECK_NODE)
           end
         else
           raise "Unhandled reflection: #{value.macro}"
@@ -201,41 +203,41 @@ module ActiveRecord::SqlExporter
       return tree
     end
     # ----------------------------------------------------------- print_relation
-    def _print_relation( tree, classes_to_ignore, indent_depth = 0 )
-      if tree[self.class.name].nil? || ( tree[self.class.name] && ( tree[self.class.name][id].nil? || tree[self.class.name][id][:type] == EXISTENCE_CHECK_NODE ) )
+    def _print_relation(tree, classes_to_ignore, indent_depth = 0)
+      if tree[self.class.name].nil? || (tree[self.class.name] && (tree[self.class.name][id].nil? || tree[self.class.name][id][:type] == EXISTENCE_CHECK_NODE))
         puts "%s%s - %d" % ["\t" * indent_depth, self.class.name, self.id]
-        self.add_to_tree( tree, CREATION_NODE )
-        _print_reflection_relations( tree, self.class.reflections, classes_to_ignore, indent_depth + 1 )
+        self.add_to_tree(tree, CREATION_NODE)
+        _print_reflection_relations(tree, self.class.reflections, classes_to_ignore, indent_depth + 1)
       end
     end
     # ---------------------------------------- convert_has_many_relations_to_sql
-    def _print_reflection_relations( tree, reflections, classes_to_ignore, indent_level = 1 )
+    def _print_reflection_relations(tree, reflections, classes_to_ignore, indent_level = 1)
       reflections.each_pair do |key, value|
-        next if value.options[:dependent] && ![:destroy, :nullify].include?( value.options[:dependent] )
+        next if value.options[:dependent] && ![:destroy, :nullify].include?(value.options[:dependent])
         if value.options[:polymorphic]
-          next if classes_to_ignore.include?( send( key ).class )
+          next if classes_to_ignore.include?(send(key).class)
         else
           begin
-            next if classes_to_ignore.include?( value.klass )
+            next if classes_to_ignore.include?(value.klass)
           rescue
             raise "Problem in a #{self.class.name} with #{key} = #{value}"
           end
         end
         case value.macro
         when :has_one
-          singleton_method( key ) do |e|
-            e._print_relation( tree, classes_to_ignore, indent_level )
+          singleton_method(key) do |e|
+            e._print_relation(tree, classes_to_ignore, indent_level)
           end
         when :has_many, :has_and_belongs_to_many
-          records = send( key )
+          records = send(key)
           if value.options[:dependent] == :nullify
             records.each do |record|
-              record.add_to_tree( tree, UPDATE_NODE, key: value.primary_key_name )
+              record.add_to_tree(tree, UPDATE_NODE, key: value.primary_key_name)
               puts "%s%s [UPDATE] - %d" % ["\t" * indent_level, record.class.name, record.id]
             end
           else
             records.each do |x|
-              x._print_relation( tree, classes_to_ignore, indent_level )
+              x._print_relation(tree, classes_to_ignore, indent_level)
             end
           end
         when :belongs_to
@@ -245,8 +247,8 @@ module ActiveRecord::SqlExporter
       end
     end
     # --------------------------------------------------------- singleton_method
-    def singleton_method( key )
-      if v = self.send( key )
+    def singleton_method(key)
+      if v = self.send(key)
         yield v
       end
     end
